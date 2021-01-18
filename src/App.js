54d394edb2cb7e6a9e9fd3aa1fac58d2fe034700
Zack Cerza <zack@redhat.com>
2021-01-18 16:56:27 -0700

    Remove some CRA cruft

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 import Product from './pages/product';
 
@@ -8,18 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <Route path="/products/:product">
         <Product />
