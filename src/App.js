3ab97a2fb446d9c8ad8389ce26ca99dd163e4fc5
Zack Cerza <zack@redhat.com>
2021-01-19 10:27:33 -0700

    Add margin around app body

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
-      <Route path="/products" exact>
-        <ProductList />
-      </Route>
-      <Route path="/products/:product">
-        <Product />
-      </Route>
+      <div className="main">
+        <Route path="/products" exact>
+          <ProductList />
+        </Route>
+        <Route path="/products/:product">
+          <Product />
+        </Route>
+      </div>
     </div>
   );
 }
