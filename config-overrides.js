f54716ddee187de82c284af1d6e99b25c9e2ceae
Zack Cerza <zack@redhat.com>
2021-01-20 11:54:47 -0700

    add config-overrides.js
    to vendor @material-ui/styles

---
+++ config-overrides.js
@@ -0,0 +1,26 @@
+module.exports = function override(config, env) {
+  console.log(config);
+  if (!config.optimization.splitChunks.cacheGroups) {
+    config.optimization.splitChunks.cacheGroups = {};
+  }
+  config.optimization.splitChunks.cacheGroups.vendor = {
+    test: /[\\/]node_modules[\\/](@material-ui\/styles)[\\/]/,
+    name: 'vendor',
+    chunks: 'all',
+  }
+  /*
+  if (!config.entry) config.entry = {};
+  if (!config.entry.vendor) config.entry.vendor = [];
+  config.entry.vendor = ["@material-ui/styles"];
+
+  if (!config.plugins) config.plugins = [];
+
+  config.plugins.push(
+    (process.env.NODE_ENV === 'production') ?
+    new CopyWebpackPlugin([{from: 'src/lib/legacyLib.js'}]) :
+    new CopyWebpackPlugin([{from: 'src/lib/legacyLib.js', to: 'dist'}])
+  );
+  */
+
+  return config;
+}
