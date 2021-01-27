81f392b889ea70aab52daf0abc0cfe079509085b
Zack Cerza <zack@redhat.com>
2021-01-27 16:23:45 -0700

    App: dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import { useMemo } from 'react';
 import { Route } from 'react-router-dom';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+
 
 import './App.css';
 import ProductList from './pages/product_list';
 import Product from './pages/product';
 
-function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-  const theme = useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-        },
-      }),
-    [prefersDarkMode],
-  );
+function App(props) {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-      <CssBaseline />
-        <header className="App-header">
-        </header>
-        <div className="main">
-          <Route path="/" exact>
-            <Product name="ocs" />
-          </Route>
-          <Route path="/products" exact>
-            <ProductList />
-          </Route>
-          <Route path="/products/:product">
-            <Product />
-          </Route>
-        </div>
-      </ThemeProvider>
+      <AppBar position="static">
+        <IconButton
+          onClick={props.toggleDarkMode}
+          style={{width: 24, marginLeft: "auto", marginRight: 12 }}
+        >
+          { props.darkMode? <Brightness7Icon /> : <Brightness4Icon /> }
+        </IconButton>
+      </AppBar>
+      <div className="main">
+        <Route path="/" exact>
+          <Product name="ocs" />
+        </Route>
+        <Route path="/products" exact>
+          <ProductList />
+        </Route>
+        <Route path="/products/:product">
+          <Product />
+        </Route>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,60 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function useDarkMode () {
+  const systemDarkMode = useMediaQuery(
+    '(prefers-color-scheme: dark)',
+  );
+  const [state, setState] = useState({system: systemDarkMode});
+  if ( state.user === undefined && systemDarkMode === true ) { setState({system: true, user: true}) };
+
+  function setDarkMode (value) {
+    const newState = {...state, user: value};
+    //if ( value !== state.user ) { setState(newState) };
+    setState(newState);
+  };
+  const darkMode = state.user
+  return [darkMode, setDarkMode]
+}
+
+function Root () {
+  const [darkMode, setDarkMode] = useDarkMode();
+  const paletteType = darkMode? 'dark' : 'light';
+  const toggleDarkMode = () => {
+    setDarkMode(! darkMode);
+  };
+  if ( darkMode === undefined ) { return null };
+
+  const getTheme = () => {
+    const theme = createMuiTheme({palette: {type: paletteType}});
+    if ( darkMode ) theme.palette.background.default = "#181818";
+    return theme
+  }
+  const theme = getTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <CssBaseline />
+        <App darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      </Router>
+    </ThemeProvider>
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Root />
+  </React.StrictMode>
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
