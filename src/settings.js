eb8f87457439cc5f286dd536e73d794e702be3a1
Zack Cerza <zack@redhat.com>
2021-01-26 17:16:29 -0700

    product: Set api_url

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ const conf = {
   jenkins: {
     url: "https://storage-jenkins-csb-ceph.cloud.paas.psi.redhat.com",
     api_url: process.env.REACT_APP_API_URL ||
-      "http://localhost:3001",
+      "http://reverse-proxy-jenkins-product-browser.apps.ocp4.prod.psi.redhat.com",
     max_builds: 0,  // 0 or undefined -> unlimited
   },
   products: {
