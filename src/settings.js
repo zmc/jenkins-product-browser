afd0c9329b81eab8e67255fb274eb4cad1096e46
Zack Cerza <zack@redhat.com>
2021-03-05 17:25:46 -0700

    reverse proxy is encrypted

--- src/settings.js
+++ src/settings.js
@@ -2,7 +2,7 @@ const conf = {
   jenkins: {
     url: "https://storage-jenkins-csb-ceph.cloud.paas.psi.redhat.com",
     api_url: process.env.REACT_APP_API_URL ||
-      "http://reverse-proxy-jenkins-product-browser.apps.ocp4.prod.psi.redhat.com",
+      "https://reverse-proxy-jenkins-product-browser.apps.ocp4.prod.psi.redhat.com",
     max_builds: 0,  // 0 or undefined -> unlimited
   },
   ocs_metadata: {
