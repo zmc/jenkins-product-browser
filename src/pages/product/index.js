ce602bd7768128588560db443803fa7767123afe
Zack Cerza <zack@redhat.com>
2021-03-25 16:31:51 -0600

    Link to images on quay.io

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -298,7 +298,7 @@ function BuildContents (props) {
             >
               <Typography variant="h6">{item.name}</Typography>
               <Typography>tag: {item.tag}</Typography>
-              <Typography>image: {item.image}</Typography>
+              <Image data={item.image} />
               <Typography>NVR: {item.nvr}</Typography>
             </Box>
           ))}
@@ -307,6 +307,33 @@ function BuildContents (props) {
     </>
   )
 }
+
+function Image (props) {
+  const getImageUrl = (image) => {
+    const imageParts = image.split("/");
+    if ( imageParts[0] === "quay.io" ) {
+      var url = `https://${imageParts[0]}/repository/${imageParts[1]}`;
+      if ( imageParts[2].includes("@") ) {
+        const [name, hash] = imageParts[2].split("@");
+        url += `/${name}/manifest/${hash}`;
+      } else if ( imageParts[2].includes(":") ) {
+        const name = imageParts[2].split(":")[0];
+        url += `/${name}`;
+      }
+      return url;
+    }
+  };
+  const url = getImageUrl(props.data);
+  return (
+    <Typography>
+      image: {
+        url === undefined? props.data:
+          (<Link href={url} target="_blank">{props.data}</Link>)
+      }
+    </Typography>
+  )
+}
+
 
 function Stage (props) {
   const statuses = {
