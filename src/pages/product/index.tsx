9e73091b1a567bcfcbe6b67ffb0caf2948704488
Zack Cerza <zack@redhat.com>
2021-08-30 18:01:20 -0600

    product

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,15 +2,16 @@ import { useParams } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
 
 import VersionList from "../../components/version_list";
+import type { URLParams } from "../../App.d";
 
 export default function Product() {
-  const { product, version } = useParams();
+  const { product } = useParams() as URLParams;
   return (
     <>
       <Typography variant="h4">
         {product}: Development Version Status
       </Typography>
-      <VersionList version={version} />
+      <VersionList />
     </>
   );
 }
